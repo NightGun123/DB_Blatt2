afca39b65a61c1c9dfce2da8e02fe7fa98780366
Benjamin Klein <mail@benjaminklein.eu>
2018-12-12 13:36:19 +0100

    class Bpd hinzugefuegt

---
+++ src/Bpd.java
@@ -0,0 +1,37 @@
+public class Bpd {
+
+    /**
+     * class Bpd
+     */
+
+    // TODO Beschreibung
+
+    // Attribute
+    protected int posnr;
+    protected String kuehl;
+    protected int artnr;
+    protected String artbez;
+    protected int anzbo;
+    protected int menge;
+    protected int algrad;
+    protected boolean verpackt;
+
+    // Konstruktor
+    public Bpd(int posnr, String kuehl, int artnr, String artbez, int anzbo, int menge) {
+
+        this.posnr = posnr;
+        this.kuehl = kuehl;
+        this. artnr = artnr;
+        this.artbez = artbez;
+        this.anzbo = anzbo;
+        this.menge = menge;
+        this.verpackt = false;
+        this.algrad = 0;
+    }
+
+    // Methoden
+    public void algrad_berechnen() {
+
+        this.algrad = ((int) (this.menge * 100) / this.anzbo) + 1;
+    }
+}
